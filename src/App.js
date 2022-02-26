08c5caee7e0cc8eef32697186e2142daa8eb8ad2
Nasir <nasir.khalid.new@gmail.com>
2022-02-26 21:07:20 +0500

    added spinner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Tabs, Tab, Table, Button } from "react-bootstrap";
+import { Container, Tabs, Tab, Table, Button, Spinner } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import axiosIns from "./services/AxiosInstance";
 import axios from "axios";
@@ -15,7 +15,10 @@ function App() {
   const [showDetails, setShowDetails] = useState(false);
   const [hideOthers, setHideOthers] = useState(false);
 
+  const [loading, setLoading] = useState(false);
+
   const authenticate = async () => {
+    setLoading(true);
     await axios
       .post("https://frontend-test-api.aircall.io/auth/login/", {
         username: "nasir",
@@ -23,22 +26,39 @@ function App() {
       })
       .then((res) => {
         localStorage.setItem("token", res.data.access_token);
-      });
+        localStorage.setItem("timestamp", dayjs());
+      })
+      .then(() => setLoading(false));
   };
 
   const fetchCalls = async (offset) => {
-    await axiosIns.get(`/calls/?offset=${offset}&limit=10`).then((res) => {
-      setCalls(
-        res.data.nodes.sort(function (a, b) {
-          return a.created_at < b.created_at
-            ? -1
-            : a.created_at > b.created_at
-            ? 1
-            : 0;
-        })
-      );
-      setHasNextPage(res.data.hasNextPage);
-    });
+    setLoading(true);
+    await axiosIns
+      .get(`/calls/?offset=${offset}&limit=10`)
+      .then((res) => {
+        setCalls(
+          res.data.nodes.sort(function (a, b) {
+            return a.created_at < b.created_at
+              ? -1
+              : a.created_at > b.created_at
+              ? 1
+              : 0;
+          })
+        );
+        setHasNextPage(res.data.hasNextPage);
+      })
+      .then(() => setLoading(false));
+  };
+
+  const refreshToken = async () => {
+    setLoading(true);
+    await axiosIns
+      .post("https://frontend-test-api.aircall.io/auth/refresh-token")
+      .then((res) => {
+        localStorage.setItem("token", res.data.access_token);
+        localStorage.setItem("timestamp", dayjs());
+      })
+      .then(() => setLoading(false));
   };
 
   const nextPage = () => {
@@ -51,6 +71,19 @@ function App() {
   useEffect(() => {
     fetchCalls(offset);
   }, [offset]);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (localStorage.getItem("timestamp")) {
+        const timestamp = localStorage.getItem("timestamp");
+        console.log(timestamp);
+        if (dayjs().diff(timestamp, "minute") >= 9) {
+          refreshToken();
+        }
+      }
+    }, 30000);
+    return () => clearInterval(timer);
+  }, []);
 
   return (
     <div className="d-flex m-5">
@@ -69,26 +102,48 @@ function App() {
                   <th>Date</th>
                   <th>View Details</th>
                   <th>Archive</th>
+                  <th>Add Note</th>
                 </tr>
               </thead>
-              <tbody>
-                {calls.map((call, index) => (
-                  <tr key={index}>
-                    <td>{index + 1 + offset}</td>
-                    <td>{dayjs(call.created_at).format("D MMMM, YYYY")}</td>
-                    <td>
-                      <CallDetails call={call} />
+              {loading ? (
+                <tbody className="text-center">
+                  <tr>
+                    <td colSpan={5}>
+                      <Spinner animation="border" />
                     </td>
-                    <td>@mdo</td>
                   </tr>
-                ))}
-              </tbody>
+                </tbody>
+              ) : (
+                <tbody>
+                  {calls.map((call, index) => (
+                    <tr key={index}>
+                      <td>{index + 1 + offset}</td>
+                      <td>{dayjs(call.created_at).format("D MMMM, YYYY")}</td>
+                      <td>
+                        <CallDetails call={call} />
+                      </td>
+                      <td>@mdo</td>
+                      <td>
+                        <div className="d-grid gap-2">
+                          <Button variant="dark">Add</Button>
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              )}
             </Table>
             <Container className="d-flex gap-4">
               <Button onClick={authenticate} variant="dark">
                 Authenticate
               </Button>
-              <Button onClick={() => fetchCalls(offset)} variant="dark">
+              <Button
+                onClick={() => {
+                  setOffset(0);
+                  fetchCalls(0);
+                }}
+                variant="dark"
+              >
                 Fetch Calls
               </Button>
               <Button
